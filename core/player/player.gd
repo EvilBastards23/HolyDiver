b1b99a9418abcb1360413c7dba0b62059d66793d
EvilBastards23 <oneyepirate3@gmail.com>
2025-05-24 19:54:41 +0800

    added dash

--- core/player/player.gd
+++ core/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const JUMP_VELOCITY = 5.6
+const JUMP_VELOCITY = 7.6
 # Child node reference
 @onready var head = $head
 @onready var camera_3d = $head/Camera3D
@@ -12,20 +12,23 @@ const JUMP_VELOCITY = 5.6
 var bob_amp = 0.01
 
 
-# Constant for speeds
-const walk_speed = 8.0
-const sprint_speed = 16.0
-
 # Global variables that used in code
 var t_bob:float = 0.0
-var speed :float= 0.0
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var speed :float= 10.0
+var gravity = 12.0
 var is_jumping:bool = false
+
+var is_dashing:bool = false
+var dash_direction = Vector3.ZERO
+@export var dash_distance:float = 15.0
+@export var dash_time:float = 0.1
+var current_dash_time:float = 0.0
+
+
 
 # Remove cursor
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	pass
 	
 # Rotating camera using mouse
 func _input(event):
@@ -34,43 +37,55 @@ func _input(event):
 		head.rotate_x(-event.relative.y * sensitivity)
 		head.rotation.x = clamp(head.rotation.x, -PI/2, PI/2)
 
+
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
+	# Add gravity unless dashing
+	if not is_on_floor() and not is_dashing:
 		is_jumping = false
 		velocity.y -= gravity * delta
-		
-	# Handle sprinting
-	if is_on_floor():
-		if Input.is_action_pressed("sprint"):
-			speed = sprint_speed
-		else:
-			speed = walk_speed
-	
-	# Handle jumping
+
+	# Jump
 	if is_on_floor() and not is_jumping and Input.is_action_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
-	
-	# Get the input direction and handle the movement/deceleration.
+
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	# Calculating speed
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-	
-	# Head bobbing effect while moving
-	if is_on_floor() and velocity.length() > 0:
+
+	# Dash logic
+	if Input.is_action_just_pressed("dash") and not is_dashing and direction != Vector3.ZERO:
+		is_dashing = true
+		current_dash_time = 0.0
+		dash_direction = direction
+		camera_3d.fov = lerp(camera_3d.fov, 90.0, 0.2)
+		$SubViewportContainer/SubViewport/Camera3D/ColorRect.show()
+
+	if is_dashing:
+		current_dash_time += delta
+		
+		if current_dash_time >= dash_time:
+			is_dashing = false
+			$SubViewportContainer/SubViewport/Camera3D/ColorRect.hide()
+		else:
+			velocity = dash_direction * (dash_distance / dash_time)
+			velocity.y = 0  # Lock vertical motion during dash
+
+	# Only move normally if not dashing
+	if not is_dashing:
+		camera_3d.fov = lerp(camera_3d.fov, 75.0, 0.1)
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			velocity.z = move_toward(velocity.z, 0, speed)
+
+	# Head bobbing
+	if is_on_floor() and velocity.length() > 0 and not is_dashing:
 		t_bob += delta * velocity.length()
-		camera_3d.transform.origin = head_bob(t_bob,velocity.length(),speed)
+		camera_3d.transform.origin = head_bob(t_bob, velocity.length(), speed)
 	else:
-		# Reset head bob when not moving or in air
-		camera_3d.transform.origin = head_bob(t_bob,velocity.length(),speed)
-	
+		camera_3d.transform.origin = head_bob(t_bob, velocity.length(), speed)
+
 	move_and_slide()
 
 
@@ -89,4 +104,8 @@ func head_bob(time, velocity_length, speed):
 	
 	return pos
 
+
+	
+	
+	
 	
