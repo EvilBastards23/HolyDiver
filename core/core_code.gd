b01fbd957ff0535fda5e80269d8f61a342d2f8bc
EvilBastards23 <oneyepirate3@gmail.com>
2025-05-28 12:14:25 +0800

    enemy

---
+++ core/core_code.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+@onready var target = $CharacterBody3D  # This is your player
+var target_position = Vector3.ZERO
+
+func _process(delta):
+	target_position =target.global_transform.origin
