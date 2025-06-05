02bcf0804a7c20acef8d4caba43a6b99db80e8f5
irmuun2345 <irmuun12a@gmail.com>
2025-06-05 10:34:55 +0800

    l
    l

--- core/player/head.gd
+++ core/player/head.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var camera_3d: Camera3D = $recoil/Camera3D
+@onready var camera_3d: Camera3D = $Camera3D
 @onready var camera_3dd: Camera3D = $"../SubViewportContainer/SubViewport/Camera3D"
 @onready var sub_viewport: SubViewport = $"../SubViewportContainer/SubViewport"
 
@@ -13,5 +13,5 @@ func _ready() -> void:
 	# Set SubViewport size
 	sub_viewport.size = screen_size
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	camera_3dd.global_transform = camera_3d.global_transform
